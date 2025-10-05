880af0c67b42924618e2d3b19937ac7f87b314e8
pakcol <stivitsme@gmail.com>
2025-10-05 21:02:16 +0700

    update lagi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.anmp.studentproject"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.anmp.studentproject"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
